9769033e191acf5b56c73744e2395352d4e692d2
erenadgzl <erenadiguzellll@gmail.com>
2020-03-16 11:51:42 +0300

    register with firebase added

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import VueRouter from "vue-router";
 
 import HelloWorld from "./components/HelloWorld"
 import LoginPage from "./components/LoginPage"
+import RegisterPage from "./components/RegisterPage"
 
 Vue.use(VueRouter);
 
 const routes = [
   { path : "/", component : HelloWorld },
   { path : "/login", component : LoginPage },
+  { path : "/register", component : RegisterPage},
   { path : "*", redirect : "/"}
 ];
 
