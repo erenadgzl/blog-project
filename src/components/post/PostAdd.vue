3cca4ce5cbc5792d4a465fd39a175ed3520be508
erenadgzl <erenadiguzellll@gmail.com>
2020-03-21 23:18:58 +0300

    post insert

---
+++ src/components/post/PostAdd.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <b-container fluid="sm">
+      <b-card class="post-add-content" header="Post Add">
+        <b-form class="sm-3" @reset="onReset">
+          <b-form-row>
+            <b-col>
+              <b-form-group
+                id="input-group-1"
+                label="Title:"
+                label-for="input-1"
+              >
+                <b-form-input
+                  id="input-1"
+                  v-model="post.title"
+                  type="text"
+                  required
+                  placeholder="Enter title"
+                ></b-form-input>
+              </b-form-group>
+            </b-col>
+          </b-form-row>
+          <b-row>
+    <b-col sm="2">
+      <label for="textarea-auto-height">Post Content:</label>
+    </b-col>
+    <b-col sm="10">
+      <b-form-textarea
+        id="textarea-auto-height"
+        rows="10"
+        max-rows="20"
+        v-model="post.content"
+      ></b-form-textarea>
+    </b-col>
+  </b-row>
+          <b-form-row>
+            <b-col class="login-button-row">
+              <b-button @click="onSubmit" type="button" variant="primary">Submit</b-button>
+              <b-button type="reset" variant="danger">Reset</b-button>
+            </b-col>
+          </b-form-row>
+        </b-form>
+      </b-card>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase"
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        content: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      firebase
+        .database()
+        .ref('posts')
+        .push(this.post);
+    },
+    onReset() {
+      this.post.email = "";
+      this.post.password = "";
+    },
+    goToRegisterPage(){
+      this.$route.push('/register')
+    }
+  }
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import VueRouter from "vue-router";
 import HelloWorld from "./components/HelloWorld"
 import LoginPage from "./components/LoginPage"
 import RegisterPage from "./components/RegisterPage"
+import PostAdd from "./components/post/PostAdd"
 
 Vue.use(VueRouter);
 
@@ -11,6 +12,7 @@ const routes = [
   { path : "/", component : HelloWorld },
   { path : "/login", component : LoginPage },
   { path : "/register", component : RegisterPage},
+  { path : "/post/add", component : PostAdd},
   { path : "*", redirect : "/"}
 ];
 
